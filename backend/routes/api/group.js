014e70fc3588ab99fe5db0142d457086e67a7c4e
Nicole Magallanes <nbmagallanesflores@gmail.com>
2024-04-04 13:23:51 -0700

    Add create group route

--- backend/routes/api/group.js
+++ backend/routes/api/group.js
@@ -10,6 +10,32 @@ const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
 
+const validateCreateGroup = [
+    check('name')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 60})
+        .withMessage("Name must be 60 characters or less"),
+    check('about')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 50})
+        .withMessage("About must be 50 characters or more"),
+    check('type')
+        .exists({ checkFalsy: true })
+        .isIn(["Online", "In person"])
+        .withMessage("Type must be 'Online' or 'In person'"),
+    check('private')
+        .exists({ checkFalsy: true })
+        .isBoolean()
+        .withMessage("Private must be a boolean"),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage("City is required"),
+    check('state')
+    .exists({ checkFalsy: true })
+    .withMessage("State is required"),
+    handleValidationErrors
+];
+
 async function memberTotal(groups) {
     if (groups.length > 1) {
         for (let group of groups) {
@@ -119,6 +145,17 @@ router.get("/:groupId", async(req, res, next) => {
     
 
     return res.json(payload);
-})
+}) ;
+
+// Create group
+router.post('/', [requireAuth, validateCreateGroup], async (req, res, next) => {
+
+    const { name, about, type, private, city, state } = req.body;
+
+    const group = await Group.create({ organizerId: req.user.id ,name, about, type, private, city, state });
+
+    if (group) res.status(201);
+    return res.json(group);
+});
 
 module.exports = router;

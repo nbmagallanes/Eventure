c61352ffc8ae0a1243a626d75327c5c26bf6f15f
Nicole Magallanes <nbmagallanesflores@gmail.com>
2024-07-19 10:09:47 -0700

    Edit landing page icons

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -1,9 +1,14 @@
 import { NavLink } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import './Landing.css'
-import treesLogo from '../../../../images/trees.png';
+// import treesLogo from '../../../../images/trees.png';
 import groupLogo from '../../../../images/teamwork.png';
 import eventLogo from '../../../../images/eventLogo.png'
+import allGroupLogo from '../../../../images/group.png'
+import landscape from '../../../../images/landscape.png'
+// import { FcConferenceCall } from "react-icons/fc";
+// import { FcPlanner } from "react-icons/fc";
+// import { FcMindMap } from "react-icons/fc";
 
 export default function Landing() {
     const user = useSelector( state => state.session.user)
@@ -16,7 +21,7 @@ export default function Landing() {
                     <p>Whatever your interest, from hiking and reading to networking and skill sharing, there are thousands of people who share it on Meetup. Events are happening every day—sign up to join the fun.</p>
                 </div>
                 <div className="section-one-element image-container">
-                    <img src={treesLogo} alt='Logo with trees' />
+                    <img src={landscape} alt='Logo with trees' />
                 </div>
             </div>
             <div className="section-two-container">
@@ -25,16 +30,19 @@ export default function Landing() {
             </div>
             <div className="section-three-container">
                 <div className="section-three-element">
-                    <img src={eventLogo} alt='event logo' />
+                    {/* <FcMindMap className="landing-icons"/> */}
+                    <img src={allGroupLogo} alt='event logo' />
                     <NavLink className='link'to='/groups'>See all groups</NavLink>
                     <p>Everyone belongs in Eventure! Browse through our many groups, join any that you are interested in.</p>
                 </div>
                 <div className="section-three-element">
                     <img src={eventLogo} alt='event logo' />
+                    {/* <FcPlanner className="landing-icons resize"/> */}
                     <NavLink className='link' to='/events'>Find an event</NavLink>
-                    <p>Find local or online events happening soon. Attend an event and new memories with new and old friends!</p>
+                    <p>Find local or online events happening soon. Attend an event and make new memories with friends!</p>
                 </div>
                 <div className="section-three-element">
+                    {/* <FcConferenceCall className="landing-icons new-group"/> */}
                     <img src={groupLogo} alt='teamwork logo' />
                     <NavLink className={user ? 'link' : 'disabled-link'} to='/groups/new'>Start a new group</NavLink>
                     <p>Would like to create a new group? Follow the link above to get started and become a group organizer!</p>
